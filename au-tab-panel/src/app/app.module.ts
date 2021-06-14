b3838d70d9112a65b8a56d483177de53758f0a94
KV <anthony.kv@capgemini.com>
2021-06-14 16:53:20 +0530

    init commit html version

--- au-tab-panel/src/app/app.module.ts
+++ au-tab-panel/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 
+
+import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuFaInputComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +16,7 @@ import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: []
 })
 export class AppModule { }
