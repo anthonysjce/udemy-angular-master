6a021203ac5d200db31c4a61c745b483f93775cf
KV <anthony.kv@capgemini.com>
2021-07-03 18:55:00 +0530

    refatoring the code to remove eventlistners- memory issue

--- au-modal/src/app/au-modal/au-modal-open-on-click.directive.ts
+++ au-modal/src/app/au-modal/au-modal-open-on-click.directive.ts
@@ -1,35 +1,43 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
 import { AuModalService } from './modal.service';
 
 @Directive({
   selector: '[auModalOpenOnClick]'
 })
-export class AuModalOpenOnClickDirective {
-
+export class AuModalOpenOnClickDirective implements OnDestroy{
+  elements:HTMLBaseElement[];
   constructor(private templateRef:TemplateRef<any>,
     private viewContainer: ViewContainerRef,
     private modalService: AuModalService) { 
       this.modalService.$close.subscribe(() => this.viewContainer.clear())
     }
-
+    
+    
     @Input()
     set auModalOpenOnClick(els) {
       //els can be array or a single element
       //check if it is an array
-      let elements:HTMLBaseElement[];
+      
       if(els.length ) {
-        elements = els;
+        this.elements = els;
       } 
       else {
-        elements = [];
-        elements.push(els);
+        this.elements = [];
+        this.elements.push(els);
       }
-      elements.forEach(e1 => {
-        e1.addEventListener('click', () => {
-          this.viewContainer.clear();
-          this.viewContainer.createEmbeddedView(this.templateRef)
-        });
+      this.elements.forEach(e1 => {        
+        e1.addEventListener('click', this.clickHandler);
       });
       
+    }
+    clickHandler(){
+      console.log(this);
+      this.viewContainer.clear();
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    }
+    ngOnDestroy() {
+      this.elements.forEach(e1 => {
+        e1.removeEventListener('click', this.clickHandler);
+      });
     }
 }
