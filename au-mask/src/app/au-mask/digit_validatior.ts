23a58d53e9cbe86d682a1fc036a4f55dd125d2c6
KV <anthony.kv@capgemini.com>
2021-07-09 19:48:38 +0530

    configuring the numberRange validator

--- au-mask/src/app/au-mask/digit_validatior.ts
+++ au-mask/src/app/au-mask/digit_validatior.ts
@@ -5,11 +5,15 @@ const loweCaseValidator:DigitValidator = (char) => /[a-z]{1}/.test(char);
 const upperCaseValidator:DigitValidator = (char) => /[A-Z]{1}/.test(char);
 const anyValidator:DigitValidator = (char) => true;
 const numberRangeValidator = (maxValue:Number,char:string) => numeriValidator(char) 
-    && parseInt(char) <= maxValue;
+    && parseInt(char) <= maxValue; //number range validator is not a type of Digit validator since it has 2 parm
 
-export const maskDigitValidator:{[key:string]:DigitValidator} = {
-    '9': numeriValidator,
+export const maskDigitValidator:{[key:string]:DigitValidator} = {    
     'a': loweCaseValidator,
     'A': upperCaseValidator,
     '*': anyValidator
+}
+
+for (let i = 0; i <= 9; i++) {
+    maskDigitValidator[i] = numberRangeValidator.bind(undefined,i);// this number range validator is DigitValidator since it single parameter that is char
+    
 }
